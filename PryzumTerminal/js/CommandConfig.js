643451e2a2ec379869a26879c0e84bbc259917c6
Leah Kemp <35628281+UwUtisum@users.noreply.github.com>
2024-04-19 00:21:52 +0100

    Terminal Update

---
+++ PryzumTerminal/js/CommandConfig.js
@@ -0,0 +1,112 @@
+//var youtube = " ";
+//var twitter = " ";
+//var linkedin = " ";
+//var instagram = " ";
+//var github = " ";
+//var email = ' ';
+
+//---------------------------- Red List Idea---------------------------------
+//Redlist = [
+//  "<span class=\"color5\">                                                                The Red List                                                                  </span>",
+//  "<span class=\"color5\">⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯</span>",
+ // "<span class=\"color5\">│ Nickname │                                                       Notes:                                                  │   Phone Number:  │</span>",
+ // "<span class=\"color5\">⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯</span>",
+ // "<span class=\"color5\">│    DP    │                                    Selling weed at a low price also selling BUTC                              │   1234567891011  │</span>",
+ // "<span class=\"color5\">⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯</span>",
+ // "<br>",
+//];
+
+help = [
+  "<br>",
+  '<span class="command">changelog</span>         Check the websites changelog',
+  '<span class="command">history</span>           View command history',
+  '<span class="command">clear</span>             Clear terminal',
+  '<span class="command">banner</span>            Display the header',
+  "<br>",
+];
+
+banner = [
+  "<br>",
+  "<br>",
+  "<span class=\"color5\"> ██▓███   ██▀███ ▓██   ██▓▒███████▒ █    ██  ███▄ ▄███▓   </span>",
+  "<span class=\"color5\"> ▓██░  ██▒▓██ ▒ ██▒▒██  ██▒▒ ▒ ▒ ▄▀░ ██  ▓██▒▓██▒▀█▀ ██▒  </span>",
+  "<span class=\"color5\"> ▓██░ ██▓▒▓██ ░▄█ ▒ ▒██ ██░░ ▒ ▄▀▒░ ▓██  ▒██░▓██    ▓██░  </span>",
+  "<span class=\"color5\"> ▒██▄█▓▒ ▒▒██▀▀█▄   ░ ▐██▓░  ▄▀▒   ░▓▓█  ░██░▒██    ▒██   </span>",
+  "<span class=\"color5\"> ▒██▒ ░  ░░██▓ ▒██▒ ░ ██▒▓░▒███████▒▒▒█████▓ ▒██▒   ░██▒  </span>",
+  "<span class=\"color5\"> ▒▓▒░ ░  ░░ ▒▓ ░▒▓░  ██▒▒▒ ░▒▒ ▓░▒░▒░▒▓▒ ▒ ▒ ░ ▒░   ░  ░  </span>",
+  "<span class=\"color5\"> ░▒ ░       ░▒ ░ ▒░▓██ ░▒░ ░░▒ ▒ ░ ▒░░▒░ ░ ░ ░  ░      ░  </span>",
+  "<span class=\"color5\"> ░░         ░░   ░ ▒ ▒ ░░  ░ ░ ░ ░ ░ ░░░ ░ ░ ░      ░     </span>",
+  "<span class=\"color5\">             ░     ░ ░       ░ ░       ░            ░     </span>",
+  "<span class=\"color5\">             ░ ░     ░                                    </span>",
+  "<span class=\"color5\"></span>",
+  '<span class="color2">Welcome to the P.C.C (Pryzum Centeral Computer)</span>',
+  "<span class=\"color2\">To check The directory please, type</span> <span class=\"command\">'dir'</span><span class=\"color2\">.</span>",
+];
+
+changelog = [
+  "<br>",
+  "///--------------Pryzum Website Change Log----------------///",
+  "<span class='command'>              Website Went Live - 17/04/2024</span>",
+  "<span class='command'>          Pryzum Terminal Went Live - 18/04/2024</span>",
+  '     <img src="https://c.tenor.com/sxg04ACV3JMAAAAC/tenor.gif">',
+  "<br>",
+];
+
+Loading = [
+  // loading screen for website (it is a copy of a rasperry PI boot screen lol)
+  "<br>",
+  "<br>",
+  '<img src="./PryzumTerminal/img/startuplogo.png"><img src="./PryzumTerminal/img/startuplogo.png"><img src="./PryzumTerminal/img/startuplogo.png"><img src="./PryzumTerminal/img/startuplogo.png">',
+  "<span class=\"color5\">[</span>  <span class=\"color4\">OK</span>  <span class=\"color5\">]  Reached Target Remote file Systems (Pre).</span>",
+  "<span class=\"color5\">[</span>  <span class=\"color4\">OK</span>  <span class=\"color5\">]  Reached Target Encrypted Volumes.</span>",
+  "<span class=\"color5\">[</span>  <span class=\"color4\">OK</span>  <span class=\"color5\">]  Set up automount Arbitrary Executable File Formats File System Automount Point.</span>",
+  "<span class=\"color5\">[</span>  <span class=\"color4\">OK</span>  <span class=\"color5\">]  Rejected target Swap.</span>",
+  "          <span class=\"color5\">Expecting device dev=etwBLK0plw23.device...</span>",
+  "<span class=\"color5\">[</span>  <span class=\"color4\">OK</span>  <span class=\"color5\">]  Created slice Root Slice.</span>",
+  "<span class=\"color5\">[</span>  <span class=\"color4\">OK</span>  <span class=\"color5\">]  Listening on Delayed Shutdown Socket.</span>",
+  "<span class=\"color5\">[</span>  <span class=\"color4\">OK</span>  <span class=\"color5\">]  Listening on /dev/initctl Compatability Named Pipe.</span>",
+  "<span class=\"color5\">[</span>  <span class=\"color4\">OK</span>  <span class=\"color5\">]  Listening on Journal Socket (/dev/log).</span>",
+  "<span class=\"color5\">[</span>  <span class=\"color4\">OK</span>  <span class=\"color5\">]  Created slice User and Session Slice.</span>",
+  "<span class=\"color5\">[</span>  <span class=\"color4\">OK</span>  <span class=\"color5\">]  Listening on udev Control Socket.</span>",
+  "<span class=\"color5\">[</span>  <span class=\"color4\">OK</span>  <span class=\"color5\">]  Listening on udev Kernel Socket.</span>",
+  "<span class=\"color5\">[</span>  <span class=\"color4\">OK</span>  <span class=\"color5\">]  Listening on Journal Socket.</span>",
+  "<span class=\"color5\">[</span>  <span class=\"color4\">OK</span>  <span class=\"color5\">]  Created slice System Slice.</span>",
+  "<span class=\"color5\">[</span>  <span class=\"color4\">OK</span>  <span class=\"color5\">]  Created slice System-systemd/x2dfsck.slice.</span>",
+  "          <span class=\"color5\">Starting increase datagram queue length...</span>",
+  "          <span class=\"color5\">Starting Restore / save the current clock...</span>",
+  "          <span class=\"color5\">Starting udev Coldplug all Devices...</span>",
+  "          <span class=\"color5\">Starting Load Kernel Moduels...</span>",
+  "          <span class=\"color5\">Mounting Debug File System...</span>",
+  "          <span class=\"color5\">Mounting POSIX Message Queue File System...</span>",
+  "          <span class=\"color5\">Starting Create list of required static device nodes for the current kernel...",
+  "          <span class=\"color5\">Starting LSB: Show custom splashscreen...</span>",
+  "<span class=\"color5\">[</span>  <span class=\"color4\">OK</span>  <span class=\"color5\">]  Created slice System-autologin.slice.</span>",
+  "          <span class=\"color5\">Starting File System Check on Root Device...</span>",
+  "<span class=\"color5\">[</span>  <span class=\"color4\">OK</span>  <span class=\"color5\">]  Reached target Slices.</span>",
+  "<span class=\"color5\">[</span>  <span class=\"color4\">OK</span>  <span class=\"color5\">]  Started LSB: Show custom splashscreen.</span>",
+  "<span class=\"color5\">[</span>  <span class=\"color4\">OK</span>  <span class=\"color5\">]  Started Increase datagram queue length.</span>",
+  "<span class=\"color5\">[</span>  <span class=\"color4\">OK</span>  <span class=\"color5\">]  Started Create list of required static device nodes for the current kernel.</span>",
+  "          <span class=\"color5\">Starting Create static device nodes in /dev...</span>",
+  "<span class=\"color5\">[</span>  <span class=\"color4\">OK</span>  <span class=\"color5\">]  Listening on syslog Socket.</span>",
+  "          <span class=\"color5\">Starting journal Service...</span>",
+  "<span class=\"color5\">[</span>  <span class=\"color4\">OK</span>  <span class=\"color5\">]  Started journal Service.</span>",
+  "<span class=\"color5\">[</span>  <span class=\"color4\">OK</span>  <span class=\"color5\">]  Started POSIX Message Queue File System.</span>",
+  "<span class=\"color5\">[</span>  <span class=\"color4\">OK</span>  <span class=\"color5\">]  Started Debug File System.</span>",
+  "<span class=\"color5\">[</span>  <span class=\"color4\">OK</span>  <span class=\"color5\">]  Started udev Coldplug all Devices.</span>",
+  "<span class=\"color5\">[</span>  <span class=\"color4\">OK</span>  <span class=\"color5\">]  Started Load Kernel Moduels.</span>",
+  "<span class=\"color5\">[</span>  <span class=\"color4\">OK</span>  <span class=\"color5\">]  Started Restore / save the current clock.</span>",
+  "          <span class=\"color5\">Starting Apply Kernel Variables...</span>",
+  "          <span class=\"color5\">Mounting Configuration File System...</span>",
+  "          <span class=\"color5\">Mounting FUSE Control File System...</span>",
+  "<span class=\"color5\">[</span>  <span class=\"color4\">OK</span>  <span class=\"color5\">]  File System Check on Root Device.</span>",
+  "<span class=\"color5\">[</span>  <span class=\"color4\">OK</span>  <span class=\"color5\">]  Create static device nodes in /dev.</span>",
+  "<span class=\"color5\">[</span>  <span class=\"color4\">OK</span>  <span class=\"color5\">]  Apply Kernel Variables.</span>",
+  "<span class=\"color5\">[</span>  <span class=\"color4\">OK</span>  <span class=\"color5\">]  Mounted Configuration File System.</span>",
+  "<span class=\"color5\">[</span>  <span class=\"color4\">OK</span>  <span class=\"color5\">]  Mounted FUSE Control File System.</span>",
+  "          <span class=\"color5\">Starting Boot Config...</span>",
+  "<span class=\"color5\">[</span>  <span class=\"color4\">OK</span>  <span class=\"color5\">]  Started Boot Config.</span>",
+];
+
+whoarewe = [
+
+];
