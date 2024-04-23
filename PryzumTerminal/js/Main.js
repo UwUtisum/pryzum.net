242c2db3c5fbfb4dcc051b499950066a5c9f0f8f
Leah Kemp <35628281+UwUtisum@users.noreply.github.com>
2024-04-23 16:22:40 +0100

    added statements command

--- PryzumTerminal/js/Main.js
+++ PryzumTerminal/js/Main.js
@@ -26,6 +26,7 @@ pw=!1;let pwd=!1;var commands=[];function enterKey(e){if(181==e.keyCode&&documen
     ;case"clear":setTimeout((function(){terminal.innerHTML='<a id="before"></a>',before=document.getElementById("before")}),1);break
     ;case"banner":loopLines(banner,"",80);break
     ;case"neofetch":loopLines(neofetch,"",80);break
+    ;case"statements":loopLines(statements,"",80);break
     // -------------------un-used commands might use later on-----------------------------
     //;case"twitter":addLine("Opening Twitter...","color2",0),newTab(twitter);break
     //;case"linkedin":addLine("Opening LinkedIn...","color2",0),newTab(linkedin);break
